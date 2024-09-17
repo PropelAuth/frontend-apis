29858ece3616963a30d5ec78d3937827deb19027
Itai Levi <italev46@gmail.com>
2024-09-17 13:54:39 -0400

    feat(client): add updatePassword to useAuthApis

--- src/client.ts
+++ src/client.ts
@@ -3,6 +3,7 @@ import { AuthUrlContext } from './AuthUrlProvider'
 import { disableMfa } from './apis/mfa/disableMfa'
 import { enableMfa } from './apis/mfa/enableMfa'
 import { fetchMfaStatusWithNewSecret } from './apis/mfa/mfaStatus'
+import { updatePassword } from './apis/updatePassword'
 
 export type ApiOptions = {
     authUrl: string
@@ -21,6 +22,7 @@ export const useAuthApis = () => {
         enableMfa: enableMfa(authUrl),
         disableMfa: disableMfa(authUrl),
         fetchMfaStatusWithNewSecret: fetchMfaStatusWithNewSecret(authUrl),
+        updatePassword: updatePassword(authUrl),
         // deleteMe: deleteMe(authUrl),
         // updateEmail: updateEmail(authUrl),
         // updateUser: updateUser(authUrl),
