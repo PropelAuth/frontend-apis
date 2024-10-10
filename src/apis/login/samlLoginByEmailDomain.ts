b45012f9458a65af256cc35ca6e1f567205bba67
Itai Levi <italev46@gmail.com>
2024-10-10 10:41:12 -0400

    fix(samlLoginByEmailDomain): redirect directly instead of relying on cookie from different endpoint

--- src/apis/login/samlLoginByEmailDomain.ts
+++ src/apis/login/samlLoginByEmailDomain.ts
@@ -1,5 +1,5 @@
 import { getVisitorOrUndefined, unmatchedCase } from '../../helpers/error_utils'
-import { ErrorCode, UnexpectedErrorResponse } from '../../helpers/errors'
+import { ErrorCode, OrgNotFoundErrorResponse, UnexpectedErrorResponse } from '../../helpers/errors'
 import { Visitor, makeRequest } from '../../helpers/request'
 
 /////////////////
@@ -16,30 +16,53 @@ export type SamlLoginByEmailDomainRequest =
 /////////////////
 ///////////////// Success and Error Responses
 /////////////////
-export type SamlLoginByEmailDomainErrorResponse = UnexpectedErrorResponse
+export type SamlLoginByEmailDomainSuccessResponse = {
+    login_url: string
+}
+
+export type SamlLoginByEmailDomainErrorResponse = UnexpectedErrorResponse | OrgNotFoundErrorResponse
 
 /////////////////
 ///////////////// Visitor
 /////////////////
-type SamlLoginByEmailDomainVisitor = Visitor
+type SamlLoginByEmailDomainVisitor = Visitor & {
+    success: (data: SamlLoginByEmailDomainSuccessResponse) => void
+    orgNotFound?: (error: OrgNotFoundErrorResponse) => void
+}
 
 /////////////////
 ///////////////// The actual Request
 /////////////////
 export const samlLoginByEmailDomain = (authUrl: string) => async (request: SamlLoginByEmailDomainRequest) => {
-    return makeRequest<SamlLoginByEmailDomainVisitor, SamlLoginByEmailDomainErrorResponse>({
+    const queryParams = new URLSearchParams()
+    if ('domain' in request) {
+        queryParams.append('domain', request.domain)
+    } else {
+        queryParams.append('email', request.email)
+    }
+    return makeRequest<
+        SamlLoginByEmailDomainVisitor,
+        SamlLoginByEmailDomainErrorResponse,
+        SamlLoginByEmailDomainSuccessResponse
+    >({
         authUrl,
-        path: '/login/saml',
+        path: '/login/check',
         method: 'GET',
-        body: request,
-        responseToSuccessHandler: () => {
-            return () => {}
+        queryParams,
+        parseResponseAsJson: true,
+        responseToSuccessHandler: (response, visitor) => {
+            return () => {
+                window.location.href = response.login_url
+                visitor.success(response)
+            }
         },
         responseToErrorHandler: (error, visitor) => {
             const { error_code: errorCode } = error
             switch (errorCode) {
                 case ErrorCode.UnexpectedError:
                     return getVisitorOrUndefined(visitor.unexpectedOrUnhandled, error)
+                case ErrorCode.OrgNotFound:
+                    return getVisitorOrUndefined(visitor.orgNotFound, error)
                 default:
                     unmatchedCase(errorCode)
                     return undefined
